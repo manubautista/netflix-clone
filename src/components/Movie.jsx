b83836244d584449c6766015bda358f9a4ef49d2
manubautista <manubautista2009@gmail.com>
2022-08-20 15:25:04 -0300

    account page and saving movies

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 import { useState } from 'react';
-
+import {UserAuth} from '../context/AuthContext'
+import { db } from '../firebase';
+import {arrayUnion, doc, updateDoc} from 'firebase/firestore'
+import { async } from '@firebase/util';
 const Movie = ({item}) => {
-    const [like, setLike] = useState([]);
+    const [like, setLike] = useState(false);
+    const [saved, setSaved] = useState(false)
+    const {user} = UserAuth()
+
+    const movieID= doc(db, 'users', `${user?.email}`)
+
+    const saveMovie = async () => {
+      if(user?.email){
+        setLike(!like)
+        setSaved(true)
+        await updateDoc(movieID, {
+          savedShows: arrayUnion({
+            id: item.id,
+            title: item.title,
+            img: item.backdrop_path
+          })
+        })
+      } else {
+          alert('Please log in to save a movie')
+      }
+    }
+
   return (
     <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[200px] inline-block cursor-pointer relative p-2">
     <img
@@ -15,11 +39,11 @@ const Movie = ({item}) => {
       <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
         {item?.title}
       </p>
-      <p>
+      <p onClick={saveMovie}>
         {like ? (
-          <FaRegHeart className="absolute top-4 text-gray300" />
-        ) : (
           <FaHeart className="absolute top-4 text-gray300" />
+        ) : (
+          <FaRegHeart className="absolute top-4 text-gray300" />
         )}
       </p>
     </div>
--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import SavedMovies from '../components/SavedMovies';
 
 const Account = () => {
   return (
-    <div>Account</div>
-  )
-}
+    <>
+      <div className='w-full text-white'>
+        <img
+          className='w-full h-[400px] object-cover'
+          src='https://assets.nflxext.com/ffe/siteui/vlv3/0f07b807-7be1-457d-be88-eb9153d5d4e9/e899568a-fb20-40c9-b376-04949af811a7/AR-es-20220815-popsignuptwoweeks-perspective_alpha_website_large.jpg'
+          alt='/'
+        />
+        <div className='bg-black/60 fixed top-0 left-0 w-full h-[550px]'></div>
+        <div className='absolute top-[20%] p-4 md:p-8'>
+          <h1 className='text-3xl md:text-5xl font-bold'>Account</h1>
+        </div>
+      </div>
+      <SavedMovies />
+    </>
+  );
+};
 
-export default Account
+export default Account;
