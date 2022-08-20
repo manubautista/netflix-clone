403958a0de192845d4bf433913917e3d05664737
manubautista <manubautista2009@gmail.com>
2022-08-20 11:21:31 -0300

    login and protectedRoute for account page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Account from "./pages/Account";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 
 function App() {
@@ -18,7 +19,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/account" element={<Account />} />
+          <Route path="/account" element={<ProtectedRoute><Account /></ProtectedRoute>} />
         </Routes>
       </AuthContextProvider>
     </>
